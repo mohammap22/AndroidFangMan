24226d678acf3b7ed50f48f35c0e4cac799b1189
vegdahl <vegdahl@up.edu>
2016-09-14 22:28:51 -0700

    Initial commit.

---
+++ app/src/main/java/edu/up/cs301/fangman/FangManActivity.java
@@ -0,0 +1,72 @@
+package edu.up.cs301.fangman;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+/**
+ * activity class for the Fang Man game
+ *
+ * @author **** put your name here ****
+ * @author **** put date of completion here ****
+ *
+ */
+public class FangManActivity extends AppCompatActivity {
+
+    /**
+     * standard 'onCreate' method
+     *
+     * @param savedInstanceState saved instance state
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // invoke superclass constructor
+        super.onCreate(savedInstanceState);
+
+        // load layout
+        setContentView(R.layout.activity_fang_man);
+
+    }
+
+    /**
+     * standard create-options menu method
+     *
+     * @param menu the menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_fang_man, menu);
+        return true;
+    }
+
+    /**
+     * standard callback method for menu-item selection
+     *
+     * @param item the item selected
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
